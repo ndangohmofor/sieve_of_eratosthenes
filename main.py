9281ab66e1377c65283e7d002f1ab2353f0e49d7
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-18 05:21:03 -0500

    Added optimized version of sieve_of_eratosthenes

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def sieve_of_eratosthenes(limit):
     output = [True] * (limit + 1)
     output[0] = False
@@ -14,8 +17,29 @@ def sieve_of_eratosthenes(limit):
 
 def find_true_indices(inputs):
     true_indices = []
-    [true_indices.append(i) for i, v in enumerate(inputs) if v]
+    [true_indices.append(i) for i, v in list(enumerate(inputs)) if v]
     return true_indices
 
 
+def sieve_of_eratosthenes_optimized(limit):
+    if limit <= 1:
+        return []
+
+    output = [True] * (limit + 1)
+    output[0] = False
+    output[1] = False
+
+    for i in range(2, math.floor(math.sqrt(limit))):
+        if output[i]:
+            j = i ** 2
+            while j <= limit:
+                output[j] = False
+                j += i
+
+    output_with_indices = list(enumerate(output))
+    trues = [idx for (idx, value) in output_with_indices if value]
+    return trues
+
+
 print(sieve_of_eratosthenes(13))
+print(sieve_of_eratosthenes_optimized(13))
